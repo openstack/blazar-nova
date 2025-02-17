527b19a65b4cab520033d777ed8e825f78dd6ed4
Pierre Riteau <pierre@stackhpc.com>
2025-02-17 22:46:59 +0100

    CI: Install nova from local git tree
    Remove zuul-cloner usage: this tool was removed after the migration to
    Zuul v3.
    
    Change-Id: I6f39986bcfe2c1229bb1fd53773013bcc57098d0

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -13,7 +13,7 @@
 # default:
 # pip install {opts} {packages}
 
-ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
+BRANCH_NAME=${NOVA_BRANCH:-master}
 nova_installed=$(echo "import nova" | python 2>/dev/null ; echo $?)
 NOVA_DIR=$HOME/nova
 
@@ -49,22 +49,12 @@ elif [ $nova_installed -eq 0 ]; then
     echo "ALREADY INSTALLED at $location"
 
     echo "Nova already installed; using existing package"
-elif [ -x "$ZUUL_CLONER" ]; then
-    echo "ZUUL CLONER" > /tmp/tox_install.txt
-    # Make this relative to current working directory so that
-    # git clean can remove it. We cannot remove the directory directly
-    # since it is referenced after $install_cmd -e.
-    mkdir -p .tmp
-    NOVA_DIR=$(/bin/mktemp -d -p $(pwd)/.tmp)
-    pushd $NOVA_DIR
-    $ZUUL_CLONER --cache-dir \
-        /opt/git \
-        --branch $BRANCH_NAME \
-        https://opendev.org \
-        openstack/nova
-    cd openstack/nova
-    $install_cmd -e .
-    popd
+elif [ -d "/opt/git/opendev.org/openstack/nova" ]; then
+    echo "PIP LOCAL" > /tmp/tox_install.txt
+    if [ -z "$NOVA_PIP_LOCATION" ]; then
+        NOVA_PIP_LOCATION="git+file:///opt/git/opendev.org/openstack/nova@$BRANCH_NAME#egg=nova"
+    fi
+    $install_cmd -U -e ${NOVA_PIP_LOCATION}
 else
     echo "PIP HARDCODE" > /tmp/tox_install.txt
     if [ -z "$NOVA_PIP_LOCATION" ]; then
